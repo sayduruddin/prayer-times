d9c8988d398cb8bd736cff2eea1bdaa20db399d3
sayduruddin <138212268+sayduruddin@users.noreply.github.com>
2026-05-10 21:54:09 +0100

    Still requires cleanup but test for sunrise added

--- backend/prayer-times/src/main/java/com/sayduruddin/prayer_times/astronomy/PrayerTimeCalculator.java
+++ backend/prayer-times/src/main/java/com/sayduruddin/prayer_times/astronomy/PrayerTimeCalculator.java
@@ -12,6 +12,32 @@ public class PrayerTimeCalculator {
         return 720 - ( 4 * longitude ) - EqT;
     }
 
+    public static double calculateHourAngle(double angle, double latitude, double declination) {
+        double angleRad = Math.toRadians(angle);
+        double latRad = Math.toRadians(latitude);
+        double declinationRad = Math.toRadians(declination);
+
+        double numerator = Math.sin(angleRad) - (Math.sin(latRad) * Math.sin(declinationRad));
+        double denominator = Math.cos(latRad) * Math.cos(declinationRad);
+
+        double cosH = numerator / denominator;
+
+        // need to add a guard for if cosH is greater than 1 or less than -1 but not sure what this means for UK
+        /*
+        * In Keighley (53.9°N) in late June:
+
+            The sun sets around 9:30pm
+            It only dips to about 13° below the horizon at its lowest point
+            If your Isha angle is -15°, the sun never reaches it — it starts rising again before getting that deep
+            cosH comes out as -1.something — outside the valid range
+
+            In Mecca (21.4°N) this never happens — the sun always dips well below the horizon even in summer.
+
+        * */
+
+        return Math.toDegrees(Math.acos(cosH));
+    }
+
     public static String formatMinutesToTime(double minutes) {
         long totalMinutes = (Math.round(minutes) + 1440) % 1440;
         long hours = totalMinutes / 60;
--- backend/prayer-times/src/test/java/com/sayduruddin/prayer_times/astronomy/PrayerTimeCalculatorTest.java
+++ backend/prayer-times/src/test/java/com/sayduruddin/prayer_times/astronomy/PrayerTimeCalculatorTest.java
@@ -18,4 +18,55 @@ public class PrayerTimeCalculatorTest {
         assertEquals("12:04", result);
     }
 
+    @Test
+    void calculateHourAngleForSunriseKeighley5thMay2026() {
+        double sunriseAngle = -0.833;
+//        double latitude = 53.8684021;
+        double latitude = 53.8675;
+        double actualLongitude = -1.9069;
+        // TODO: add in a function that calculates JD for you
+        double T = SolarPosition.calculateJulianCentury(2461165.5);
+
+        double obliquity = SolarPosition.calculateObliquity(T);
+        double meanLongitude = SolarPosition.calculateMeanLongitude(T);
+        double omega =  SolarPosition.calculateOmega(T);
+        double meanAnomaly = SolarPosition.calculateMeanAnomaly(T);
+
+        double equationOfCentre = SolarPosition.calculateEquationOfCentre(meanAnomaly);
+        double trueLongitude = SolarPosition.calculateTrueLongitude(meanLongitude, equationOfCentre);
+
+        double appLongitude =  SolarPosition.calculateApparentLongitude(trueLongitude, omega);
+
+        double declination = SolarPosition.calculateDeclination(obliquity, appLongitude);
+
+
+        double result = PrayerTimeCalculator.calculateHourAngle(sunriseAngle, latitude, declination);
+//        assertEquals(114.24957096919297, result, 1e-5);
+
+//        double actualLongitude = -1.9078;
+
+        // mean longitude required for equation of time.
+        double EqT = SolarPosition.calculateEquationOfTime(T, obliquity, meanAnomaly, meanLongitude);
+        double solarNoon = PrayerTimeCalculator.calculateSolarNoon(EqT, actualLongitude);
+
+        double offsetFromSolarNoon = result * 4;
+        double sunriseInMinutes = solarNoon - offsetFromSolarNoon;
+
+        System.out.println(PrayerTimeCalculator.formatMinutesToTime(sunriseInMinutes));
+        System.out.println("T: " + T);
+        System.out.println("Mean Longitude: " + meanLongitude);
+        System.out.println("Mean Anomaly: " + meanAnomaly);
+        System.out.println("Obliquity: " + obliquity);
+        System.out.println("Omega: " + omega);
+        System.out.println("Apparent Longitude: " + SolarPosition.calculateApparentLongitude(meanLongitude, omega));
+        System.out.println("Declination: " + declination);
+        System.out.println("EqT: " + EqT);
+        System.out.println("Solar Noon UTC minutes: " + solarNoon);
+        System.out.println("Solar Noon UTC: " + PrayerTimeCalculator.formatMinutesToTime(solarNoon));
+        System.out.println("Hour Angle: " + result);
+        System.out.println("Offset minutes: " + (result * 4));
+        System.out.println("Sunrise UTC minutes: " + sunriseInMinutes);
+        System.out.println("Sunrise UTC: " + PrayerTimeCalculator.formatMinutesToTime(sunriseInMinutes));
+    }
+
 }
